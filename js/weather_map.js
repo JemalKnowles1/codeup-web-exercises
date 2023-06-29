b984f06ffd87bab17f4baaedf61dfb3c5ff284d7
Kofie Knowles <JemalKnowles1@gmail.com>
2023-06-29 14:49:43 -0400

    Weather Proj

--- js/weather_map.js
+++ js/weather_map.js
@@ -29,28 +29,6 @@ setInterval(() => {
     timeE1.innerHTML = hoursIn12HrFormat + ':' + minutes + ' ' + `<span id="am-pm">${ampm}</span>`
     dateE1.innerHTML = days[day] + ', ' + date + ' ' + months[month]
 
-
 }, 1000);
 
-/**
- * gets current location of user and returns local weather
- */
-function getInfo() {
-    const newName = document.getElementById("cityInput")
-    const cityName = document.getElementById('cityName')
-    cityName.innerHTML = "--"+newName.value+"--"
-}
-// fetch(`https://api.openweathermap.org/data/2.5/
-// weather?q='+newName.value+'&APPID=${API_KEY}`).then(response =>response.json()).then(data =>{
-//     for(i=0 ; i<5 ; i++){
-//         document.getElementById("day"+(i+1)+"Min").innerHTML
-//             ="Min:" +Number(data.list[i].main.temp_min -288.53).toFixed(1)
-//
-//             ="Max:" +Number(data.list[i].main.temp_min -288.53).toFixed(1)
-//     }
-// })
 
-/**
- *  grabs current timezone and country data while returning and updating weather
- * @param data
- */
